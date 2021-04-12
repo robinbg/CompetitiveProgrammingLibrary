9f389b436ee804827921b6f1ffa0fd8c83f413b1
Biaolin Wen <robinbg@foxmail.com>
2021-04-12 11:13:09 +0800

    Create Inv.cpp

---
+++ NumberTheory/Inv.cpp
@@ -0,0 +1,12 @@
+
+inv[1] = 1;
+for (int i = 2; i <= n; ++i) {
+  inv[i] = (long long)(p - p / i) * inv[p % i] % p;
+}
+
+//O(n)求任意n个数的逆元
+
+inv[1] = 1;
+for (int i = 2; i <= n; ++i) {
+  inv[i] = (long long)(p - p / i) * inv[p % i] % p;
+}
